4cea1dbee780c3ee4ca3815933e6251d4e348fb4
Alexander Missaoui <alexandermissaoui@gmail.com>
2022-10-18 00:18:26 +0200

    no refresh

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-form.addEventListener('submit', e => {
-    e.preventDefault()
-  
-    if(firstName.value.trim() === '' || 
-    lastName.value.trim() === '' || 
-    email.value.trim() === '' ||
-    password.value.trim() === '' ||
-    repeatPassword.value.trim() === '' ){
-      console.log('du måste fylla i alla fält')
-      return
-    }
-  
-  
-    const user = new User(firstName.value, lastName.value, email.value, password.value, repeatPassword.value)
-  
-    users.push(user)
-    console.log(users)
-    form.reset()
+document.getElementById("validationForm").addEventListener("submit",function(e){
+  e.preventDefault()
 
-    })
+  alert("Validationform is submitted!")
+})
